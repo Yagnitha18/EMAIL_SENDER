543c19ef38c0c7b04903fd8d5b46589ec023eca2
Yagnitha <98573889+Yagnitha18@users.noreply.github.com>
2024-11-18 19:19:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ DB_CONFIG = {
     'host': 'localhost',
     'database': 'emailsender',
     'user': 'postgres',
-    'password': 'yagnitha',
+    'password': 'password',
     'port': 5432
 }
 
 SMTP_CONFIG = {
-    'email': 'yagni780@gmail.com',
-    'password': 'tayp zntq roet uupb ',
+    'email': 'email@gmail.com',
+    'password': 'app - password ',
     'server': 'smtp.gmail.com',
     'port': 587
 }
